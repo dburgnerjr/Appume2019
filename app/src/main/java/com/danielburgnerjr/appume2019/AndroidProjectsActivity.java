a71bf52f5d3787b67423321bc3192fa00eafd213
Daniel Burgner <dburgnerjr@yahoo.com>
2019-04-27 23:01:07 -0500

    D27 ExperienceActivity/AndroidProjectsActivity Part 1
    D27 of #100DaysOfCode #2019 challenge.  Started buildout of
    ExperienceActivity and AndroidProjectsActivity for #AppumeV2 app.
    #UnfinishedBusiness #AndroidDev

---
+++ app/src/main/java/com/danielburgnerjr/appume2019/AndroidProjectsActivity.java
@@ -0,0 +1,6 @@
+package com.danielburgnerjr.appume2019;
+
+import android.app.Activity;
+
+public class AndroidProjectsActivity extends Activity {
+}
---
+++ app/src/main/java/com/danielburgnerjr/appume2019/ExperienceActivity.java
@@ -0,0 +1,6 @@
+package com.danielburgnerjr.appume2019;
+
+import android.app.Activity;
+
+public class ExperienceActivity extends Activity {
+}
